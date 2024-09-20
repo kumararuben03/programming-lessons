a1796bbe498fe65c3c2feccd21e6b7dabcbdcda5
kumararuben03 <kumararuben0306@gmail.com>
2024-09-20 21:43:10 +0800

    Update Todo List App

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -85,6 +85,86 @@
   </v-app>
 </template>
 
-<script src="./assets/scripts.js"></script>
+<script>
+export default {
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+    };
+  },
 
-<style src="./assets/styles.css"></style>
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.tasks.push({
+          text: this.newTask,
+          completed: false,
+          editable: false, // Add editable property
+        });
+        this.newTask = "";
+      }
+    },
+
+    handleEnterKey(event) {
+      if (event.keyCode === 13) {
+        this.addTask();
+      }
+    },
+
+    editTask(index) {
+      this.tasks[index].editable = true;
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    saveTask(index) {
+      this.tasks[index].editable = false;
+    },
+  },
+
+  computed: {
+    percentageCompleted() {
+      const totalTasks = this.tasks.length;
+      const completedTasks = this.tasks.filter((task) => task.completed).length;
+      return totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
+    },
+
+    progressBarColor() {
+      return this.percentageCompleted === 100 ? "success" : "error";
+    },
+    progressBarStyle() {
+      return { "background-size": `${this.percentageCompleted}%` };
+    },
+  },
+};
+</script>
+
+<style>
+.v-card-title .v-btn {
+  margin-left: 5px;
+}
+
+.list-btns {
+  display: flex;
+  justify-content: space-between;
+}
+
+.edit-btns {
+  display: flex;
+  gap: 10px;
+}
+
+.v-progress-linear {
+  width: 100%;
+  font-weight: bolder;
+}
+
+@media only screen and (max-width: 600px) {
+  .v-progress-linear {
+    margin: 0 auto;
+  }
+}
+</style>
