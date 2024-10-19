b36c700b3de5873a1fcb09c0dc2f831834c180f6
kumararuben03 <kumararuben0306@gmail.com>
2024-10-20 00:33:38 +0800

    Update

--- Week 7/database.py
+++ Week 7/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_POST")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
